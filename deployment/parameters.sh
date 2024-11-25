1d22bac78516df2f9c36a771e1344e11484c32c0
Omer Cauly <omerca@cloudzone.io>
2024-11-25 17:29:45 +0200

    oleg is the king

---
+++ deployment/parameters.sh
@@ -0,0 +1,29 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://d-9367b67736.awsapps.com/start
+REGION=eu-west-1
+TEAM_ACCOUNT=867344455507
+ORG_MASTER_PROFILE=team_iam_master
+TEAM_ACCOUNT_PROFILE=team_iam
+TEAM_ADMIN_GROUP="TEAM_ADMINS"
+TEAM_AUDITOR_GROUP="TEAM_AUDITORS"
+TAGS="project=iam-identity-center-team environment=testing"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:eu-west-1:867344455507:eventdatastore/1ce6cb00-6140-4e76-87fb-846aad065c0a
+SECRET_NAME=TEAM-IDC-APP
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
+
+
